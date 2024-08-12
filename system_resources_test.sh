a78fb1dd3cf0286a08c55dd4d9959b9bc6bb04d6
jonwang22 <jonwang.kuralabs@gmail.com>
2024-08-12 05:37:34 +0000

    Adding framework to resources script

---
+++ system_resources_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# This script is going to retrieve all system resources and have an exit code when one of the resources exceeds a set threshold.
+
+# Setting threshold for CPU, Memory, Disk Storage
+CPU_THRESHOLD=70       # Percentage
+MEMORY_THRESHOLD=80    # Percentage
+DISK_THRESHOLD=70      # Percentage
+
+# Initializing resource threshold exceeded limits.
+CPU_EXCEEDED=0
+MEMORY_EXCEEDED=0
+DISK_EXCEEDED=0
+
+# Check CPU function
+function check_cpu (){
+	
+}
+
+
+
+# Check Memory function
+function check_mem (){
+
+}
+
+
+
+
+# Check Disk function
+function check_disk (){
+
+}
+
+
+
+
+
